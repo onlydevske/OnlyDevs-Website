ccde5ab85052018849993661ce0424c93b0b7318
morachake <jacobmoracha@gmail.com>
2023-11-22 20:11:48 +0300

    updates on team

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -55,26 +55,26 @@ const teamMembers = [
 export default function OurTeam() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {teamMembers.map((member) => (
-          <div key={member.name} className="bg-white shadow-lg rounded-lg overflow-hidden">
-            <div className="p-4 border-b-4 border-blue-600 rounded-full mx-auto w-32 h-32 overflow-hidden">
-              <img className="w-full h-full object-cover rounded-full" src={member.imageUrl} alt={member.name} />
-            </div>
-            <div className="p-4">
-              <h3 className="text-lg font-semibold">{member.name}</h3>
-              <p className="text-sm">{member.role}</p>
-              <p className="text-gray-600 mt-1">{member.bio}</p>
-              <div className="flex mt-4 space-x-3">
-                <a href={member.social.facebook} target="_blank" rel="noopener noreferrer"><FaFacebookF className="text-blue-600" /></a>
-                <a href={member.social.twitter} target="_blank" rel="noopener noreferrer"><FaTwitter className="text-blue-400" /></a>
-                <a href={member.social.github} target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-                <a href={member.social.linkedin} target="_blank" rel="noopener noreferrer"><FaLinkedinIn className="text-blue-500" /></a>
-              </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-center">
+      {teamMembers.map((member) => (
+        <div key={member.name} className="bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
+          <div className="p-4 border-b-8 border-yellow-200 rounded-full mx-auto w-40 h-40 overflow-hidden shadow-xl">
+            <img className="w-full h-full object-cover rounded-full" src={member.imageUrl} alt={member.name} style={{ boxShadow: '0 4px 6px rgba(250, 225, 0, 0.5), 0 2px 4px rgba(250, 225, 0, 0.6)' }} />
+          </div>
+          <div className="p-4">
+            <h3 className="text-lg font-semibold">{member.name}</h3>
+            <p className="text-sm">{member.role}</p>
+            <p className="text-gray-600 mt-1">{member.bio}</p>
+            <div className="flex justify-center mt-4 space-x-3">
+              <a href={member.social.facebook} target="_blank" rel="noopener noreferrer"><FaFacebookF className="text-yellow-600" /></a>
+              <a href={member.social.twitter} target="_blank" rel="noopener noreferrer"><FaTwitter className="text-yellow-500" /></a>
+              <a href={member.social.github} target="_blank" rel="noopener noreferrer"><FaGithub className="text-gray-700" /></a>
+              <a href={member.social.linkedin} target="_blank" rel="noopener noreferrer"><FaLinkedinIn className="text-blue-700" /></a>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
+  </div>
   );
 }
--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Stories = () => {
+    // Sample images, replace with actual images for each story
+    const storyImages = {
+        story1: '/path/to/story1-image.jpg',
+        story2: '/path/to/story2-image.jpg',
+        story3: '/path/to/story3-image.jpg'
+    };
+
     return (
         <section className="bg-white py-8">
             <div className="container mx-auto flex flex-wrap">
@@ -9,53 +16,62 @@ const Stories = () => {
                 </h2>
                 {/* Story 1 */}
                 <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-                    <div className="h-full bg-gray-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                            FROM BUGS TO FEATURES
-                        </h2>
-                        <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                            Jane Doe's Journey with Open Source
-                        </h1>
-                        <p className="leading-relaxed mb-3">
-                            Jane shares her incredible journey from being a novice in programming to a contributor to major open-source projects. Find out how our community helped her achieve her goals.
-                        </p>
-                        <a className="text-indigo-500 inline-flex items-center">
-                            Learn More
-                        </a>
+                    <div className="h-full bg-gray-100 rounded-lg overflow-hidden text-center relative">
+                        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={storyImages.story1} alt="Story 1" />
+                        <div className="p-6">
+                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                                FROM BUGS TO FEATURES
+                            </h2>
+                            <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                                Jane Doe's Journey with Open Source
+                            </h1>
+                            <p className="leading-relaxed mb-3">
+                                Jane shares her incredible journey from being a novice in programming to a contributor to major open-source projects.
+                            </p>
+                            <a className="text-indigo-500 inline-flex items-center">
+                                Learn More
+                            </a>
+                        </div>
                     </div>
                 </div>
                 {/* Story 2 */}
                 <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-                    <div className="h-full bg-gray-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                            THE CODE WARRIOR
-                        </h2>
-                        <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                            Alex's 30 Days of Code Challenge
-                        </h1>
-                        <p className="leading-relaxed mb-3">
-                            Alex took on a coding challenge that not only improved his skills but also landed him his dream job. Dive into his month-long coding adventure.
-                        </p>
-                        <a className="text-indigo-500 inline-flex items-center">
-                            Learn More
-                        </a>
+                    <div className="h-full bg-gray-100 rounded-lg overflow-hidden text-center relative">
+                        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={storyImages.story2} alt="Story 2" />
+                        <div className="p-6">
+                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                                THE CODE WARRIOR
+                            </h2>
+                            <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                                Alex's 30 Days of Code Challenge
+                            </h1>
+                            <p className="leading-relaxed mb-3">
+                                Alex took on a coding challenge that not only improved his skills but also landed him his dream job.
+                            </p>
+                            <a className="text-indigo-500 inline-flex items-center">
+                                Learn More
+                            </a>
+                        </div>
                     </div>
                 </div>
                 {/* Story 3 */}
                 <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-                    <div className="h-full bg-gray-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                            DESIGN MEETS CODE
-                        </h2>
-                        <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                            Michael's UI/UX Breakthrough
-                        </h1>
-                        <p className="leading-relaxed mb-3">
-                            Discover how Michael bridged the gap between design and development, creating stunning, functional websites, and how OnlyDevs provided the platform for his portfolio.
-                        </p>
-                        <a className="text-indigo-500 inline-flex items-center">
-                            Learn More
-                        </a>
+                    <div className="h-full bg-gray-100 rounded-lg overflow-hidden text-center relative">
+                        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={storyImages.story3} alt="Story 3" />
+                        <div className="p-6">
+                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                                DESIGN MEETS CODE
+                            </h2>
+                            <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                                Michael's UI/UX Breakthrough
+                            </h1>
+                            <p className="leading-relaxed mb-3">
+                                Discover how Michael bridged the gap between design and development, creating stunning, functional websites.
+                            </p>
+                            <a className="text-indigo-500 inline-flex items-center">
+                                Learn More
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
