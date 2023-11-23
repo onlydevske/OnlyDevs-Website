5edeabd36029a28a6a4e86f9920ecc22c4582b74
morachake <jacobmoracha@gmail.com>
2023-11-23 23:45:27 +0300

    updating recommendation

--- src/pages/Forum.jsx
+++ src/pages/Forum.jsx
@@ -43,15 +43,7 @@ const recommended = [
         <DiscussionsList/>
 
           {/* Recommended Sidebar */}
-          <div className="w-1/4 bg-white p-4 rounded-md shadow-md">
-            <h4 className="font-bold mb-4">Recommended</h4>
-            {recommended.map((discussion) => (
-              <div key={discussion.id} className="mb-2">
-                <h5 className="text-indigo-600 hover:text-indigo-800 cursor-pointer">{discussion.title}</h5>
-                <p className="text-xs">Upvotes: {discussion.upvotes}</p>
-              </div>
-            ))}
-          </div>
+         
         </div>
       </div>
     </div>
