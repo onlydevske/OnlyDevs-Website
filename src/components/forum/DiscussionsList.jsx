89c0d7f4055250e4a1ee5438496ef46a72df45fa
morachake <jacobmoracha@gmail.com>
2023-11-23 23:44:22 +0300

    discussion data

--- src/components/forum/DiscussionsList.jsx
+++ src/components/forum/DiscussionsList.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+
+const discussions = [
+    { id: 1, title: 'How to integrate TypeScript in a React project?', upvotes: 42, category: 'TypeScript', replies: 8 },
+    { id: 2, title: 'Best practices for state management in Vue.js', upvotes: 35, category: 'Vue.js', replies: 5 },
+    { id: 3, title: 'Has anyone tried the new Deno runtime?', upvotes: 27, category: 'Deno', replies: 3 },
+  
+  ];
 export default function DiscussionsList() {
   return (
     <>
